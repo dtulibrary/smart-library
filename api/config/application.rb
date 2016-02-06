f0463cdf3290e55f2e2d5ccaaffc8c8c102f1a49
Viktor Smari <viktorsmari@gmail.com>
2016-02-06 18:15:41 +0100

    Allow angular to get data from api

--- api/config/application.rb
+++ api/config/application.rb
@@ -19,7 +19,10 @@ module Smartlib
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'http://192.168.1.64:8100',
+    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+  }
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
