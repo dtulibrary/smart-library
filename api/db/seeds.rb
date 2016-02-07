4aac1a48b5c127de5e71f3095db072933e85356b
Viktor Smari <viktorsmari@gmail.com>
2016-02-07 13:09:57 +0100

    Add gitignore and seeds file

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Location.delete_all
+Location.create(name:'East');
+Location.create(name:'West');
+Location.create(name:'North');
+Location.create(name:'South');
+
+Sensor.delete_all
+8.times do |s|
+  Sensor.create!(
+    tagname:"Sensor#{s}", 
+    location: Location.offset(rand(Location.count)).first,
+    install_date: rand(3.years).seconds.ago
+  )
+end
+
+Value.delete_all
+5.times do |v|
+  Value.create!(temp:rand(18..25), sound:rand(25..85), light:rand(400..800), sensor: Sensor.offset(rand(Sensor.count)).first);
+end
+
+
+
