ae7576510a22b5f8db9dee4719fbc4a22dc6133e
Viktor Smari <viktorsmari@gmail.com>
2016-02-21 17:30:09 +0100

    Show values in admin

--- api/app/admin/value.rb
+++ api/app/admin/value.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Value do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :temp, :sound, :light, :sensor_id
 #
 # or
 #
@@ -12,6 +12,18 @@ ActiveAdmin.register Value do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-
+index do
+  selectable_column
+  column :id
+  column :temp
+  column :sound
+  column :light
+  column :created_at
+  column :updated_at
+  column "Sensor" do |x|
+    link_to x.sensor_id, admin_sensor_path(x.sensor_id)
+  end
+  
+end
 
 end
--- api/app/models/sensor.rb
+++ api/app/models/sensor.rb
@@ -1,6 +1,3 @@
 class Sensor < ActiveRecord::Base
   belongs_to :location
-  def viktor
-    "#{tagname} - (#{id})"
-  end
 end
