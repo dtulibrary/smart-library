e0ec571a87cfaee5fe4399cf183c83f2fa8984a6
Viktor Smari <viktorsmari@gmail.com>
2016-02-24 22:20:20 +0100

    Show individual values json

--- api/app/controllers/values_controller.rb
+++ api/app/controllers/values_controller.rb
@@ -2,4 +2,8 @@ class ValuesController < ApplicationController
   def index
     @values = Value.all
   end
+
+  def show
+    @value = Value.find(params[:id])
+  end
 end
---
+++ api/app/views/values/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @value, :id, :temp, :sound, :light
